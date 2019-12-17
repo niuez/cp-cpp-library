c99f148ea9e926741d96477ffd6e6c2c3a16f726
niuez <niuez.graph@gmail.com>
2019-12-18 00:11:17 +0900

    [update] lazy segment tree (not recusive)

--- src/data_structures/segment_tree/lazy_segment_tree.hpp
+++ src/data_structures/segment_tree/lazy_segment_tree.hpp
@@ -1,72 +1,109 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <vector>
+#include <set>
+#include <iostream>
 using i64 = long long;
 
 struct lazy_segment_tree {
   using T = i64;
-  using L = pair<i64, i64>;
-  static T t_ide() { return 0; }
-  static L l_ide() { return {0, 0}; }
-  static T ope(const T& a, const T& b) { return a + b; }
-  static L lazy_ope(const L& a, const L& b) { return { a.first + b.first, a.second + b.second }; }
-  static T effect(const T& t, const L& l, const i64 len) { return t + l.first * len; }
+  using L = i64;
+  static inline T t_ide() { return (1LL << 31) - 1; }
+  static inline L l_ide() { return (1LL << 31) - 1; }
+  static inline T ope(const T& a, const T& b) { return std::min(a, b); }
+  static inline L lazy_ope(const L& a, const L& b) { return b; }
+  static inline T effect(const T& t, const L& l) { return l; }
 
-  i64 n;
-  vector<T> node;
-  vector<L> lazy;
-  vector<bool> flag;
+  int n, h;
+  std::vector<T> node;
+  std::vector<L> lazy;
+  std::vector<bool> flag;
 
-  /* create lazy_segment_tree for sequence [t_ide; sz]A */
-  lazy_segment_tree(const vector<T>& init) {
+  lazy_segment_tree(int N) {
     n = 1;
-    while(n < init.size()) n *= 2;
-    node.resize(2 * n - 1, t_ide());
-    lazy.resize(2 * n - 1, l_ide());
-    flag.resize(2 * n - 1, false);
-    for(int i = 0;i < init.size();i++) node[i + n - 1] = init[i];
-    for(int i = n - 2; i >= 0;i--) node[i] = ope(node[i * 2 + 1], node[i * 2 + 2]);
+    h = 1;
+    while(n < N) n <<= 1, h++;
+    node.resize(n << 1, t_ide());
+    lazy.resize(n << 1, l_ide());
+    flag.resize(n << 1, false);
   }
-  
-
-  void eval(i64 l, i64 r, i64 k) {
-    if(flag[k]) {
-      node[k] = effect(node[k], lazy[k], r - l);
-      
-      if(r - l > 1) {
-        lazy[k * 2 + 1] = lazy_ope(lazy[k * 2 + 1], lazy[k]);
-        flag[k * 2 + 1] = true;
-        lazy[k * 2 + 2] = lazy_ope(lazy[k * 2 + 2], lazy[k]);
-        flag[k * 2 + 2] = true;
-      }
-
-      flag[k] = false;
-      lazy[k] = l_ide();
-    }
+  lazy_segment_tree(const std::vector<T>& init) {
+    n = 1;
+    h = 1;
+    while(n < init.size()) n <<= 1, h++;
+    node.resize(n << 1, t_ide());
+    lazy.resize(n << 1, l_ide());
+    flag.resize(n << 1, false);
+    for(int i = 0;i < init.size();i++) node[i + n] = init[i];
+    for(int i = n; i --> 1;) node[i] = ope(node[(i << 1)], node[(i << 1) + 1]);
   }
-  
-  /* for i in [a, b) effect(a[i], lx) */
-  void update(i64 a, i64 b, L lx, i64 l = 0, i64 r = -1, i64 k = 0) {
-    if(r < 0) r = n;
-    eval(l, r, k);
-    if(b <= l || r <= a) return;
-    else if(a <= l && r <= b) {
-      lazy[k] = lazy_ope(lazy[k], lx);
+
+  inline void eff(int k, L x) {
+    if(k < n << 1) {
+      lazy[k] = lazy_ope(lazy[k], x);
       flag[k] = true;
-      eval(l, r, k);
-    }
-    else {
-      update(a, b, lx, l, (l + r) / 2, k * 2 + 1);
-      update(a, b, lx, (l + r) / 2, r, k * 2 + 2);
-      node[k] =ope(node[k * 2 + 1], node[k * 2 + 2]);
     }
   }
-  
-  /* the sum of a[i] for i in [a, b) */
-  T sum(i64 a, i64 b, i64 l = 0, i64 r = -1, i64 k = 0) {
-    if(r < 0) r = n;
-    eval(l, r, k);
-    if(b <= l || r <= a) return t_ide();
-    else if(a <= l && r <= b) return node[k];
-    else return ope(sum(a, b, l, (l + r) / 2, k * 2 + 1), sum(a, b, (l + r) / 2, r, k * 2 + 2));
+  inline T eval(int k) const { return flag[k] ? effect(node[k], lazy[k]) : node[k]; }
+
+  inline void push(int k) {
+    if(flag[k]) {
+      node[k] = eval(k);
+      eff(k << 1, lazy[k]);
+      eff((k << 1) | 1, lazy[k]);
+      lazy[k] = l_ide();
+      flag[k] = false;
+    }
+  }
+
+  inline void infuse(int k) {
+    k = k >> __builtin_ctz(k);
+    while((k >>= 1)) node[k] = ope(eval(k << 1), eval((k << 1) + 1));
+  }
+
+  inline void infiltrate(int k) {
+    if(k == n << 1) return;
+    int kc = __builtin_ctz(k);
+    for(int i = h; i --> kc;) push(k >> i);
+  }
+
+  inline void infiltrate(int l, int r) {
+    if(r == n << 1) infiltrate(l);
+    else {
+      int hh = h;
+      int x = l ^ r;
+      for(; !(x >> --hh);) push(l >> hh);
+      int lc = __builtin_ctz(l);
+      for(int i = hh + 1; i --> lc;) push(l >> i);
+      int rc = __builtin_ctz(r);
+      for(int i = hh + 1; i --> rc;) push(r >> i);
+    }
+  }
+
+  void update(int a, int b, L x) {
+    int l = a + n;
+    int r = b + n;
+    infiltrate(l, r);
+    while(l < r) {
+      if(l & 1) eff(l++, x);
+      if(r & 1) eff(--r, x);
+      l >>= 1;
+      r >>= 1;
+    }
+    infuse(a + n);
+    infuse(b + n);
+  }
+
+  T sum(int l, int r) {
+    l += n;
+    r += n;
+    infiltrate(l, r);
+    T lx = t_ide();
+    T rx = t_ide();
+    while(l < r) {
+      if(l & 1) lx = ope(lx, eval(l++));
+      if(r & 1) rx = ope(eval(--r), rx);
+      l >>= 1;
+      r >>= 1;
+    }
+    return ope(lx, rx);
   }
 };
