520cdf06cafca1c9d61adce2b23bfe85b9787845
niuez <niuez.graph@gmail.com>
2019-12-19 09:53:13 +0900

    [update] fastio (operator>> and << define macro)

--- src/other/fastio.hpp
+++ src/other/fastio.hpp
@@ -7,18 +7,21 @@ namespace niu {
     inline char next() { return cur = getc_unlocked(stdin); }
     inline char peek() { return cur; }
     inline void skip() { while(is_blank(next())){} }
-    FIN& operator>>(int& n) {
-      bool sign = 0;
-      n = 0;
-      skip();
-      while(!is_blank(peek())) {
-        if(peek() == '-') sign = 1;
-        else n = (n << 1) + (n << 3) + (peek() & 0b1111);
-        next();
-      }
-      if(sign) n = -n;
-      return *this;
+#define intin(inttype)  \
+    FIN& operator>>(inttype& n) { \
+      bool sign = 0; \
+      n = 0; \
+      skip(); \
+      while(!is_blank(peek())) { \
+        if(peek() == '-') sign = 1; \
+        else n = (n << 1) + (n << 3) + (peek() & 0b1111); \
+        next(); \
+      } \
+      if(sign) n = -n; \
+      return *this; \
     }
+intin(int)
+intin(long long)
   } fin;
 
   char tmp[128];
@@ -27,18 +30,21 @@ namespace niu {
     inline void push(char c) { putc_unlocked(c, stdout); }
     FOUT& operator<<(char c) { push(c); return *this; }
     FOUT& operator<<(const char* s) { while(*s) push(*s++); return *this; }
-    FOUT& operator<<(int n) {
-      if(n) {
-        char* p = tmp + 127; bool neg = 0;
-        if(n < 0) neg = 1, n = -n;
-        while(n) *--p = (n % 10) | 0b00110000, n /= 10;
-        if(neg) *--p = '-';
-        return (*this) << p;
-      }
-      else {
-        push('0');
-        return *this;
-      }
+#define intout(inttype) \
+    FOUT& operator<<(inttype n) { \
+      if(n) { \
+        char* p = tmp + 127; bool neg = 0; \
+        if(n < 0) neg = 1, n = -n; \
+        while(n) *--p = (n % 10) | 0b00110000, n /= 10; \
+        if(neg) *--p = '-'; \
+        return (*this) << p; \
+      } \
+      else { \
+        push('0'); \
+        return *this; \
+      } \
     }
+intout(int)
+intout(long long)
   } fout;
 }
