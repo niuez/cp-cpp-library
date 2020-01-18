edcbf9e8eb285de8a2ad7665f8b93eed79ef27c6
niuez <niuez.graph@gmail.com>
2020-01-19 01:51:20 +0900

    [update] segment_tree

--- src/data_structures/segment_tree/segment_tree.hpp
+++ src/data_structures/segment_tree/segment_tree.hpp
@@ -1,47 +1,42 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <vector>
 using i64 = long long;
- 
+
+template<class T, class Ope, const T& Ide>
 struct segment_tree {
-  using T = i64;
-  static T ope(const T& a, const T& b) {
-    return max(a, b);
-  }
-  static T ide() { return -1e18; }
+  Ope ope;
  
   i64 n;
-  vector<T> node;
+  std::vector<T> node;
  
-  segment_tree(const vector<T>& init) {
+  segment_tree(const std::vector<T>& init) {
     n = 1;
     while(n < init.size()) n *= 2;
-    node.resize(2 * n, ide());
+    node.resize(2 * n, Ide);
     for(int i = 0;i < init.size();i++) node[i + n] = init[i];
     for(int i = n - 1; i >= 1;i--) node[i] = ope(node[i * 2], node[i * 2 + 1]);
   }
  
-  void update(i64 i, T x) {
+  void modify(i64 i, T x) {
     i += n;
-    node[i] = x;
+    node[i] = ope(node[i], x);
     while(i > 1) {
       i = i / 2;
       node[i] = ope(node[i * 2], node[i * 2 + 1]);
     }
   }
-  
+
   /* [l, r) */
-  T sum(i64 l, i64 r) {
-    T lx = ide();
-    T rx = ide();
+  T sum(i64 l, i64 r) const {
+    T lx = Ide;
+    T rx = Ide;
     l += n;
-    r += n - 1;
+    r += n;
     while(l < r) {
-      if(l & 1) { lx = ope(lx, node[l]); }
-      if(!(r & 1)) { rx = ope(node[r], rx); }
-      l = (l + 1) >> 1;
-      r = (r - 1) >> 1;
+      if(l & 1) { lx = ope(lx, node[l++]); }
+      if(r & 1) { rx = ope(node[--r], rx); }
+      l >>= 1;
+      r >>= 1;
     }
-    if(l == r) { lx = ope(lx, node[l]); }
     return ope(lx, rx);
   }
 };
