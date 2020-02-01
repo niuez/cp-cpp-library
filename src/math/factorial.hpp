f57b53644f23f886790f79ec3c15fce900c5f29d
niuez <niuez.graph@gmail.com>
2020-02-01 17:11:33 +0900

    [fix] factorial

--- src/math/factorial.hpp
+++ src/math/factorial.hpp
@@ -5,7 +5,7 @@ template<class T>
 void build_factorial(std::vector<T>& fact, std::vector<T>& finv, std::vector<T>& inv) {
   std::size_t N = fact.size();
   fact[0] = T(1);
-  for(int i = 0;i < N;i++) {
+  for(int i = 1;i < N;i++) {
     fact[i] = fact[i - 1] * T(i);
   }
   finv[N - 1] = T(1) / fact[N - 1];
