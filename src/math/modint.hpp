544192e860099b9aa705373c5744e0ef61c50321
niuez <niuez.graph@gmail.com>
2020-04-08 18:00:40 +0900

    [update] modint compressed

--- src/math/modint.hpp
+++ src/math/modint.hpp
@@ -1,88 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 using i64 = long long;
-
-template<i64 M>
-constexpr i64 euinv(i64 val) {
-    i64 a = M, b = val;
-    i64 x = 0, u = 1;
-    while (b) {
-        i64 t = a / b;
-        swap(a -= t * b, b);
-        swap(x -= t * u, u);
-    }
-    return x < 0 ? x + M : x;
-}
-
-template<i64 M>
-struct modint {
-  i64 a;
-  constexpr modint(const i64 x = 0) noexcept: a((x % M + M) % M) {}
-  constexpr i64 value() const noexcept { return a; }
-  constexpr modint inv() const noexcept { return modint(euinv<M>(a)); }
-  constexpr modint pow(i64 r) const noexcept {
-    modint ans(1);
-    modint aa = *this;
-    while(r) {
-      if(r & 1) {
-        ans *= aa;
-      }
-      aa *= aa;
-      r >>= 1;
-    }
+template<i64 M> struct modint { i64 a;
+  constexpr modint(const i64 x = 0): a((x%M+M)%M){}
+  constexpr i64 value() const { return a; }
+  constexpr modint inv() const { return this->pow(M-2); }
+  constexpr modint pow(i64 r) const {
+    modint ans(1); modint aa = *this;
+    while(r) { if(r & 1) ans *= aa; aa *= aa; r >>= 1; }
     return ans;
   }
-  constexpr modint& operator+=(const modint r) noexcept {
-    a += r.a;
-    if(a >= M) a -= M;
-    return *this;
-  }
-  constexpr modint& operator=(const i64 r) {
-    a = (r % M + M) % M;
-    return *this;
-  }
-  constexpr modint& operator-=(const modint r) noexcept {
-    a -= r.a;
-    if(a < 0) a += M;
-    return *this;
-  }
-  constexpr modint& operator*=(const modint r) noexcept {
-    a = a * r.a % M;
-    return *this;
-  }
-  constexpr modint& operator/=(modint r) noexcept {
-    i64 ex = M - 2;
-    while(ex) {
-      if(ex & 1) {
-        *this *= r;
-      }
-      r *= r;
-      ex >>= 1;
-    }
-    return *this;
-  }
-
-  constexpr modint operator+(const modint r) const {
-    return modint(*this) += r;
-  }
-  constexpr modint operator-(const modint r) const {
-    return modint(*this) -= r;
-  }
-  constexpr modint operator*(const modint r) const {
-    return modint(*this) *= r;
-  }
-  constexpr modint operator/(const modint r) const {
-    return modint(*this) /= r;
-  }
-
-  constexpr bool operator!=(const modint r) const {
-    return this->value() != r.value();
-  }
-
+  constexpr modint& operator=(const i64 r) { a = (r % M + M) % M; return *this; }
+  constexpr modint& operator+=(const modint r) { a += r.a; if(a >= M) a -= M; return *this; }
+  constexpr modint& operator-=(const modint r) { a -= r.a; if(a < 0) a += M; return *this; }
+  constexpr modint& operator*=(const modint r) { a = a * r.a % M; return *this; }
+  constexpr modint& operator/=(const modint r) { (*this) *= r.inv(); return *this; }
+  constexpr modint operator+(const modint r) const { return modint(*this) += r; }
+  constexpr modint operator-(const modint r) const { return modint(*this) -= r; }
+  constexpr modint operator*(const modint r) const { return modint(*this) *= r; }
+  constexpr modint operator/(const modint r) const { return modint(*this) /= r; }
+  constexpr bool operator!=(const modint r) const { return this->value() != r.value(); }
 };
 
-template<const i64 M>
-std::ostream& operator<<(std::ostream& os, const modint<M>& m) {
-  os << m.value();
-  return os;
-}
+template<const i64 M> std::ostream& operator<<(std::ostream& os, const modint<M>& m) { os << m.value(); return os; }
+
