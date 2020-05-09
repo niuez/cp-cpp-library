bba7daea138111394f7e1bd2292a27dd84c195bc
niuez <niuez.graph@gmail.com>
2020-05-09 10:38:56 +0900

    [add] gt more fast

--- src/graph/max_flow/goldberg_tarjan_global_relabeling.hpp
+++ src/graph/max_flow/goldberg_tarjan_global_relabeling.hpp
@@ -16,6 +16,7 @@ struct goldberg_tarjan {
   std::vector<int> h;
   std::vector<std::vector<int>> que;
   std::vector<int> qi;
+  std::vector<int> ei;
   const cap_type INF = 1e18;
 
   goldberg_tarjan(int n): N(n), G(n) {}
@@ -34,13 +35,14 @@ struct goldberg_tarjan {
   }
 
   void relabel(int v) {
-    h[v] = N - 1;
-    for(const auto& e: G[v]) {
-      if(e.cap > 0 && h[v] > h[e.to]) {
-        h[v] = h[e.to];
+    h[v] = N;
+    for(int i = 0; i < G[v].size(); i++) {
+      auto& e = G[v][i];
+      if(e.cap > 0 && h[v] > h[e.to] + 1) {
+        ei[v] = i;
+        h[v] = h[e.to] + 1;
       }
     }
-    h[v]++;
   }
 
   std::vector<int> Q;
@@ -48,6 +50,7 @@ struct goldberg_tarjan {
     for(int i = 0;i < N;i++) {
       que[i].clear();
       qi[i] = 0;
+      ei[i] = 0;
     }
     int i = 0, qr = 0;
     Q[qr++] = t;
@@ -79,6 +82,7 @@ struct goldberg_tarjan {
 
     que.assign(N, std::vector<int>());
     qi.assign(N, 0);
+    ei.assign(N, 0);
 
     global_relabeling(t);
 
@@ -88,12 +92,14 @@ struct goldberg_tarjan {
       if(qi[di] == que[di].size()) { di--; continue; }
       int v = que[di][qi[di]++];
       if(exc[v] == 0 || v == t) continue;
-      for(auto& e: G[v]) {
+      for(int& i = ei[v]; i < G[v].size(); i++) {
+        auto& e = G[v][i];
         if(e.cap > 0 && h[v] == h[e.to] + 1) {
           push(v, e);
           if(exc[e.to] > 0 && e.to != t) {
             que[h[e.to]].push_back(e.to);
           }
+          if(exc[v] == 0) break;
         }
       }
       if(exc[v] == 0) continue;
@@ -110,64 +116,3 @@ struct goldberg_tarjan {
     return exc[t];
   }
 };
-
-
-#include <bits/stdc++.h>
-using namespace std;
-using i64 = long long;
-#define rep(i,s,e) for(i64 (i) = (s);(i) < (e);(i)++)
-#define all(x) x.begin(),x.end()
-
-template<class T>
-static inline std::vector<T> ndvec(size_t&& n, T val) noexcept {
-  return std::vector<T>(n, std::forward<T>(val));
-}
-
-template<class... Tail>
-static inline auto ndvec(size_t&& n, Tail&&... tail) noexcept {
-  return std::vector<decltype(ndvec(std::forward<Tail>(tail)...))>(n, ndvec(std::forward<Tail>(tail)...));
-}
-
-template<class T, class Cond>
-struct chain {
-  Cond cond; chain(Cond cond) : cond(cond) {}
-  bool operator()(T& a, const T& b) const {
-    if(cond(a, b)) { a = b; return true; }
-    return false;
-  }
-};
-template<class T, class Cond>
-chain<T, Cond> make_chain(Cond cond) { return chain<T, Cond>(cond); }
-
-int main() {
-  cin.tie(nullptr);
-  std::ios::sync_with_stdio(false);
-  int H, W;
-  cin >> H >> W;
-
-  goldberg_tarjan gt(H + W + 2);
-  int s = H + W;
-  int t = s + 1;
-  vector<i64> A(H, 0);
-  rep(i,0,H) rep(j,0,W) {
-    i64 g;
-    cin >> g;
-    A[i] += g;
-    gt.add_edge(i, H + j, g);
-  }
-  i64 sum = 0;
-  rep(i,0,H) {
-    i64 r;
-    cin >> r;
-    i64 MIN = std::min(A[i], r);
-    sum += r - MIN;
-    gt.add_edge(s, i, A[i] - MIN);
-  }
-  rep(i,0,W) {
-    i64 r;
-    cin >> r;
-    sum += r;
-    gt.add_edge(H + i, t, r);
-  }
-  cout << sum - gt.max_flow(s, t) << "\n";
-}
