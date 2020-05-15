05193af8b1f0a19f467e4104320097e7b13aecbe
niuez <niuez.graph@gmail.com>
2020-05-15 10:49:52 +0900

    [fix] union find

--- src/data_structures/union_find/union_find.hpp
+++ src/data_structures/union_find/union_find.hpp
@@ -1,29 +1,22 @@
 #include <vector>
-#include <set>
-using namespace std;
+#include <tuple>
 
 struct union_find {
-  vector<int> par;
-  vector<int> rank;
-  union_find(int n) : par(n) , rank(n) {
-    for(int i = 0;i < n;i++) par[i] = i;
+  std::vector<int> par;
+  union_find(int N): par(N, -1) {}
+  int root(int x) {
+    return par[x] < 0 ? x : par[x] = root(par[x]);
   }
-  int root(int i) {
-    return par[i] == i ? i : par[i] = root(par[i]);
-  }
-  /* unite x, y return parent */
-  int unite(int x,int y) {
+  std::tuple<int, int> unite(int x, int y) {
     x = root(x);
     y = root(y);
-    if(x == y) return -1;
-    if(rank[x] < rank[y]) {
-      par[x] = y;
-      return y;
-    }
-    else {
-      par[y] = x;
-      if(rank[x] == rank[y]) rank[x]++;
-      return x;
-    }
+    if(x == y) return { -1, -1 };
+    if(par[x] > par[y]) std::swap(x, y);
+    par[x] += par[y];
+    par[y] = x;
+    return { x, y };
+  }
+  int size(int x) {
+    return -par[root(x)];
   }
 };
