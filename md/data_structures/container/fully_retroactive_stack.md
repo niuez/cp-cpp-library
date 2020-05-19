04f394c0fb2143fe930865693df7198114811bd4
niuez <niuez.graph@gmail.com>
2020-05-19 09:14:34 +0900

    [add] frs and sp md

---
+++ md/data_structures/container/fully_retroactive_stack.md
@@ -0,0 +1,7 @@
+# Fully Retroactive Stack
+
+### Code
+
+```cpp
+{{#include ../../../src/data_structures/container/fully_retroactive_stack.hpp}}
+```
--- src/data_structures/sparse_table/sparse_table.hpp
+++ src/data_structures/sparse_table/sparse_table.hpp
@@ -3,33 +3,30 @@ using namespace std;
 using i64 = long long;
 
 struct sparse_table {
-  using Band = i64;
-  Band ope(const Band& a, const Band b) { return min(a, b); }
+  using Band = int;
+  inline Band ope(const Band& a, const Band b) { return min(a, b); }
 
   i64 N;
-  vector<Band> A;
-  vector<i64> log_t;
   vector<vector<Band>> table;
 
-  sparse_table(vector<Band> arr) : N(arr.size()), A(arr), log_t(arr.size() + 1) {
-    for(int i = 2;i < N + 1;i++) {
-      log_t[i] = log_t[i >> 1] + 1;
-    }
-    table.resize(N, vector<Band>(log_t[N] + 1));
+  sparse_table(vector<Band> arr) : N(arr.size()) {
+    table.resize(__lg(N) + 1);
 
+    table[0].resize(N);
     for(int i = 0;i < N;i++) {
-      table[i][0] = arr[i];
+      table[0][i] = arr[i];
     }
 
     for(int k = 1;(1 << k) <= N;k++) {
+      table[k].resize(N);
       for(int i = 0;i + (1 << k) <= N;i++) {
-        table[i][k] = ope(table[i][k - 1], table[i + (1 << (k - 1))][k - 1]);
+        table[k][i] = ope(table[k - 1][i], table[k - 1][i + (1 << (k - 1))]);
       }
     }
   }
   /* [s, t) */
   Band query(i64 s, i64 t) {
-    i64 k = log_t[t - s];
-    return ope(table[s][k], table[t - (1 << k)][k]);
+    int k = __lg(t - s);
+    return ope(table[k][s], table[k][t - (1 << k)]);
   }
 };
