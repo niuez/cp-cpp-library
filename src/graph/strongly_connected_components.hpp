d622830bbaec1059ce5647bdc963012f919f7840
niuez <niuez@localhost.localdomain>
2020-09-19 19:04:16 +0900

    [add] scc

--- src/graph/strongly_connected_components.hpp
+++ src/graph/strongly_connected_components.hpp
@@ -1,43 +1,61 @@
-#include <bits/stdc++.h>
-using namespace std;
-using i64 = long long;
+#include <vector>
 
-vector<i64> strongly_connected_components(const vector<vector<i64>>& g) {
-  i64 n = g.size();
-  vector<vector<i64>> rg(n);
-  for(int i = 0;i < n;i++) {
-    for(auto j: g[i]) {
-      rg[j].push_back(i);
+struct strongly_connected_componects {
+  int n;
+  std::vector<std::vector<int>> G;
+  std::vector<std::vector<int>> scc;
+  std::vector<int> low;
+  std::vector<int> num;
+  std::vector<int> st;
+  std::vector<bool> in_st;
+  int cnt;
+
+  strongly_connected_componects(int n):
+    n(n), G(n) {}
+
+  void add_edge(int a, int b) {
+    G[a].emplace_back(b);
+  }
+
+  void visit(int v) {
+    low[v] = num[v] = ++cnt;
+    st.emplace_back(v);
+    in_st[v] = true;
+
+    for(auto t: G[v]) {
+      if(num[t] == 0) {
+        visit(t);
+        low[v] = std::min(low[v], low[t]);
+      }
+      else if(in_st[t]) {
+        low[v] = std::min(low[v], num[t]);
+      }
+    }
+
+    if(low[v] == num[v]) {
+      std::vector<int> vs;
+      while(true) {
+        int t = st.back();
+        st.pop_back();
+        in_st[t] = false;
+        vs.emplace_back(t);
+        if(v == t) break;
+      }
+      scc.emplace_back(std::move(vs));
     }
   }
 
-  vector<bool> vis(n,false);
-  vector<i64> vs;
-  vector<i64> res(n,-1);
-  function<void(int)> dfs = [&](int v) {
-    vis[v] = true;
-    for(auto& t : g[v]) {
-      if(!vis[t]) dfs(t);
+  void build_scc() {
+    low.assign(n, 0);
+    num.assign(n, 0);
+    st.reserve(n);
+    in_st.assign(n, false);
+    cnt = 0;
+    for(int i = 0;i < n;i++) {
+      if(num[i] == 0) {
+        visit(i);
+      }
     }
-    vs.push_back(v);
-  };
-
-  function<void(int,int)> rdfs = [&](int v,int k) {
-    vis[v] = true;
-    res[v] = k;
-    for(auto to : rg[v]) {
-      if(!vis[to]) rdfs(to,k);
-    }
-  };
-
-  for(int i = 0;i < n;i++) {
-    if(!vis[i]) dfs(i);
   }
+};
 
-  vis.assign(n,false);
-  int k = 0;
-  for(int i = n - 1;i >= 0;i--) {
-    if(!vis[vs[i]]) rdfs(vs[i] , k++);
-  }
-  return res;
-}
