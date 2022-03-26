1cd47bac48d0a15f593537c42236041f39ff2a06
niuez <niuez.graph@gmail.com>
2022-03-26 16:29:57 +0900

    [add] matrix

--- src/math/matrix.hpp
+++ src/math/matrix.hpp
@@ -1,24 +1,54 @@
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
-#define rep(i,s,e) for(i64 (i) = (s);(i) < (e);(i)++)
+using i64 = long long;
 
 template<class T>
 struct matrix {
   int N, M;
   vector<vector<T>> mat;
-  matrix(int N, int M, T init = T()): N(N), M(M), mat(N, vector<T>(M, init)) {}
+  matrix(int N, int M, T init = T::zero()): N(N), M(M), mat(N, vector<T>(M, init)) {}
   vector<T>& operator[](i64 i) { return mat[i]; }
   const vector<T>& operator[](i64 i) const { return mat[i]; }
   static matrix<T> E(int N) {
-    matrix<T> mat(N, N, 0);
-    rep(i,0,N) mat[i][i] = 1;
+    matrix<T> mat(N, N, T::zero());
+    for(int i = 0; i < N; i++) {
+      mat[i][i] = T::one();
+    }
     return mat;
   }
 };
+
+template<class T>
+matrix<T> operator+(const matrix<T>& lm, const matrix<T>& rm) {
+  matrix ret(lm.N, lm.M);
+  for(int i = 0; i < lm.N; i++) {
+    for(int j = 0; j< lm.M; j++) {
+      ret[i][j] = lm[i][j] + rm[i][j];
+    }
+  }
+  return ret;
+}
 template<class T>
 matrix<T> operator*(const matrix<T>& lm, const matrix<T>& rm) {
   matrix<T> ret(lm.N, rm.M);
-  rep(i, 0, lm.N) rep(j, 0, lm.M) rep(k, 0, rm.M) (ret[i][k] += lm[i][j] * rm[j][k]);
+  for(int i = 0; i < lm.N; i++) {
+    for(int j = 0; j < lm.M; j++) {
+      for(int k = 0; k < rm.M; k++) {
+        ret[i][k] = ret[i][k] + lm[i][j] * rm[j][k];
+      }
+    }
+  }
+  return ret;
+}
+
+template<class T>
+matrix<T> operator*(const matrix<T>& lm, const T& r) {
+  matrix<T> ret(lm.N, lm.M);
+  for(int i = 0; i < lm.N; i++) {
+    for(int j = 0; j< lm.M; j++) {
+      ret[i][j] = lm[i][j] * r;
+    }
+  }
   return ret;
 }
  
